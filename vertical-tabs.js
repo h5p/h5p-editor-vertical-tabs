03899187dbeb1e4060cb48db56f37fcd04bcd072
Håkon Ellingsen <hakonweb@gmail.com>
2016-07-06 10:09:14 +0200

    HVP-79: Fix arrow glitch and placeholder border

--- vertical-tabs.js
+++ vertical-tabs.js
@@ -208,6 +208,10 @@ H5PEditor.VerticalTabs = (function ($) {
           }
         }).insertBefore($tab);
 
+        $('<div/>', {
+          class: 'h5p-vtab-a',
+        }).appendTo($placeholder);
+
         move(event);
         return false;
       };
